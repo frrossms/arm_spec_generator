f9942544c1b071aa3d001a8a1235b70df55bdb6f
Fred Ross <frross@microsoft.com>
2021-05-28 12:03:44 -0700

    Reformat example.ts

--- example/example.ts
+++ example/example.ts
@@ -1,45 +1,42 @@
 import {
-    Version, VersionKind, TargetVersion,
-    Module,
-    ResourceType,
-    StringT,
-    generateSwagger
+  Version,
+  VersionKind,
+  TargetVersion,
+  Module,
+  ResourceType,
+  StringT,
+  generateSwagger,
 } from "../src/main.ts";
 
 const targetVersions: Array<TargetVersion> = [
-    [[2021,3,1], VersionKind.Preview],
-    [[2021,4,1], VersionKind.Preview],
-    [[2021,4,15], VersionKind.GA]
-]
+  [[2021, 3, 1], VersionKind.Preview],
+  [[2021, 4, 1], VersionKind.Preview],
+  [[2021, 4, 15], VersionKind.GA],
+];
 
 const module: Module = {
-    filename: "example.json",
-    title: "Example",
-    description: "Example module to generate Swagger generation.",
-    namespace: "Microsoft.Example",
-    resources: [
-        {
-            previewVersion: [2021,4,1],
-           path: [
-               [
-                   "genericResources", 
-                   {
-                        name: "resourceName"
-                   }
-                ]
-            ],
-            resourceType: ResourceType.Tracked,
-            readableName: "generic resource",
-            readablePluralName: "generic resources",
-            properties: {
-                "field1": {
-                    previewVersion: [2021,4,1],
-                    description: "First field of the resource.",
-                    type: StringT
-                }
-            }
-        }
-    ]
+  filename: "example.json",
+  title: "Example",
+  description: "Example module to generate Swagger generation.",
+  namespace: "Microsoft.Example",
+  resources: [
+    {
+      previewVersion: [2021, 4, 1],
+      path: [
+        ["genericResources", {name: "resourceName"}],
+      ],
+      resourceType: ResourceType.Tracked,
+      readableName: "generic resource",
+      readablePluralName: "generic resources",
+      properties: {
+        field1: {
+          previewVersion: [2021, 4, 1],
+          description: "First field of the resource.",
+          type: StringT,
+        },
+      },
+    },
+  ],
 };
 
 generateSwagger(module, "Microsoft.Example", targetVersions);
